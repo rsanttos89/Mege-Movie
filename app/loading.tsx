2d406da9a571c930fa05adf099bcb784926569c1
rsanttos89 <rsanttos.tech@gmail.com>
2024-09-01 16:02:29 -0300

    Loading...

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+  return (
+    <section style={{
+      height: '100%', 
+      overflow: 'auto',
+      padding: '16px',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <h1>Loading...</h1>
+    </section>
+  );
+}
